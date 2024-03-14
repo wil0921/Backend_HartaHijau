12fd2180a77e4406f27f3618bdd06b2a27706d51
unknown <alwyyourbae@gmail.com>
2024-03-14 23:31:04 +0700

    remove users model

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,12 +3,6 @@ import authController from "../controllers/auth.controller";
 // Import middleware
 import authMiddleware from "../middleware/auth.middleware";
 
-// Dummy data user
-const users = [
-  { id: 1, phoneNumber: "08123456789", password: "password1" },
-  { id: 2, phoneNumber: "08987654321", password: "password2" },
-];
-
 // Route register
 app.post("/register", authController.register);
 
