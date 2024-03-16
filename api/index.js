7b84ab66cf2ea91ec83865935adb7045e5b33a21
Nama Anda <emailanda@example.com>
2024-03-16 13:14:33 +0700

    database add

--- api/index.js
+++ api/index.js
@@ -2,25 +2,37 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("../src/routes/");
 const init = require('../src/config/connect')
+const mongoose = require('mongoose')
 
 const app = express();
 
-// middleware
+// Mongoose connection
+mongoose.connect('mongodb+srv://hartahijau:<mongodb@0m1>@cluster0.2t0ihsa.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+  process.exit(1);
+});
+
+// Middleware
 app.use(bodyParser.json());
 
 // menjalankan database
-init()
+init();
 
 app.get("/", (req, res) => {
   res.status(200).send("Selamat datang di server Node.js!");
 });
 
-// route
+// Routes
 app.use(routes);
 
 // jika user mengakses url yang tidak ada maka server akan mengirimkan respon berikut
 app.use("*", (req, res) => {
-  res.status(404).json({ code: 404, message: "ups something went wrong" });
+  res.status(404).json({ code: 404, message: "Ups, something went wrong" });
 });
 
 module.exports = app;
