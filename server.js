702e022aad4f7eafb2b58a3463ea603650b29f55
unknown <alwyyourbae@gmail.com>
2024-03-21 22:37:20 +0700

    move dotenv config from server.js to database.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const app = require("./api");
-require('dotenv').config();
 
 // sebaiknya kalo untuk backend pake port selain 3000, karna biasanya port 3000 udah dipake sama frontend
 const PORT = process.env.PORT !== undefined ? process.env.PORT : 88;
--- src/config/database.js
+++ src/config/database.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require("mongodb");
+require('dotenv').config();
 
 const client = new MongoClient(process.env.MONGODB_URI, {
   useNewUrlParser: true,
