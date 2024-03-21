12fb2f01ce1625c42fa81f1031ab9dbf99ef8012
unknown <alwyyourbae@gmail.com>
2024-03-21 21:50:42 +0700

    add dotenv config

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require("./api");
+require('dotenv').config();
 
 // sebaiknya kalo untuk backend pake port selain 3000, karna biasanya port 3000 udah dipake sama frontend
 const PORT = process.env.PORT !== undefined ? process.env.PORT : 88;
@@ -6,3 +7,6 @@ const PORT = process.env.PORT !== undefined ? process.env.PORT : 88;
 app.listen(PORT, () => {
   console.log(`Server sedang berjalan di http://localhost:${PORT}`);
 });
+
+// ftur 
+// transaksi(kirim, terima, struk, history), users(adduser), auth(register, login, forgotpassword), produk(getallproduk), userAdmin(updatePoin), news(createNews, getallnews), profile(foto, nama, logout), 
