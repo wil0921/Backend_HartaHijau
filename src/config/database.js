e1c18395d05c1727252470da84a7899292c8e47a
unknown <alwyyourbae@gmail.com>
2024-03-21 21:47:37 +0700

    change database uri name

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+const { MongoClient } = require("mongodb");
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connectToDatabase = async () => {
+  try {
+    await client.connect();
+    console.log("Terhubung ke database MongoDB");
+    return client.db(process.env.dbName);
+  } catch (error) {
+    console.error("Koneksi ke database gagal:", error);
+    throw error;
+  }
+};
+
+module.exports = connectToDatabase;
