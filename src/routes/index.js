51cc9020013816622631086d75beade0a51cb2b5
unknown <alwyyourbae@gmail.com>
2024-03-21 21:49:38 +0700

    add userRoute

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const routes = require("express").Router();
 
 const authRoute = require("./auth.route");
+const userRoute = require('./users.route');
 
 routes.use("/api/v1", authRoute);
+routes.use("/api/v1/users", userRoute);
 
 module.exports = routes;
