3b9c6887192e15e34f13ee2a342b88a61134255d
unknown <alwyyourbae@gmail.com>
2024-03-25 00:18:48 +0700

    change collection name, fix deleteUserById

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,19 +1,20 @@
+const connectToDatabase = require("../config/database");
+
 const addUser = async (req, res) => {
   const { phone, username, password } = req.body;
   const db = await connectToDatabase();
-  const collection = db.collection("Harta_Hijau_Data");
+  const collection = db.collection("Users");
   const newUser = { phone, username, password };
 
   try {
-    const result = await collection.insertOne(newUser);
-    res.status(201).json({
+    await collection.insertOne(newUser);
+    return res.status(201).json({
       status: true,
       message: "Berhasil menambahkan user",
-      result: result.ops[0],
     });
   } catch (error) {
     console.error("Error saat menambahkan user:", error);
-    res.status(500).json({
+    return res.status(500).json({
       status: false,
       message: "Terjadi kesalahan saat menambah user",
       error: error.message,
@@ -23,7 +24,7 @@ const addUser = async (req, res) => {
 
 const getAllUser = async (req, res) => {
   const db = await connectToDatabase();
-  const collection = db.collection("Harta_Hijau_Data");
+  const collection = db.collection("Users");
 
   try {
     const users = await collection.find().toArray();
@@ -44,15 +45,22 @@ const getAllUser = async (req, res) => {
 
 const deleteUserById = async (req, res) => {
   const db = await connectToDatabase();
-  const collection = db.collection("Harta_Hijau_Data");
-  const id = req.params.id;
+  const collection = db.collection("Users");
+  const id = req.query.id;
+  const { ObjectId } = require("mongodb");
 
   try {
-    const result = await collection.deleteOne({ _id: id });
-    res.status(200).json({
-      status: true,
-      message: `User dengan ID ${id} berhasil dihapus`,
-    });
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+
+    return result.deletedCount
+      ? res.status(200).json({
+          status: true,
+          message: `User dengan ID ${id} berhasil dihapus`,
+        })
+      : res.status(400).json({
+          status: false,
+          message: `User dengan ID ${id} tidak ditemukan`,
+        });
   } catch (error) {
     console.error("Error saat menghapus user:", error);
     res.status(500).json({
