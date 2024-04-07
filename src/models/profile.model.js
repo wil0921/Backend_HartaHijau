f4a930193155131f5bee8c78a5d1517d4b907ed4
unknown <alwyyourbae@gmail.com>
2024-04-07 13:23:56 +0700

    add profile controller

---
+++ src/models/profile.model.js
@@ -0,0 +1,30 @@
+const getUserProfileById = (id) => {
+  const query = ` SELECT user.username, user.profile_picture, poin.total_withdrawl, poin.total_earning
+                  FROM user
+                  INNER JOIN poin 
+                  ON user.id = poin.user_id
+                  WHERE user.id = ?`;
+  const values = [id];
+
+  return pool.query(query, values);
+};
+
+const getDetailUserProfileById = (id) => {
+  const query =
+    "SELECT, username, phone_number, email, createdAt FROM user WHERE id = ?";
+  const values = [id];
+
+  return pool.query(query, values);
+};
+
+const updateUserProfileById = (id, fields) => {
+  const setClause = Object.keys(fields)
+    .map((field) => `${field} = ?`)
+    .join(", ");
+  const values = Object.values(fields);
+  values.push(id);
+
+  const query = `UPDATE users SET ${setClause} WHERE id = ?`;
+
+  return pool.query(query, values);
+};
