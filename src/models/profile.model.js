7586f95e14de0d8f55710e69bc1db7c762c5f8e5
unknown <alwyyourbae@gmail.com>
2024-04-08 14:13:28 +0700

    change sql query to prisma query

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -1,38 +1,36 @@
-const prisma = require('../config/database');
+const prisma = require("../config/database");
 
-const getUserProfileById = (id) => {
-  const query = ` SELECT user.username, user.profile_picture, poin.total_withdrawl, poin.total_earning
-                  FROM user
-                  INNER JOIN poin 
-                  ON user.id = poin.user_id
-                  WHERE user.id = ?`;
-  const values = [id];
-
-  return pool.query(query, values);
+const getUserProfileById = (userId) => {
+  return prisma.profile.findUnique({
+    where: { userId },
+    select: {
+      profile_picture: true,
+      user: { select: { username: true } },
+      poin: { select: { total_withdrawl: true, total_earning: true } },
+    },
+  });
 };
 
-const getDetailUserProfileById = (id) => {
-  const query =
-    "SELECT, username, phone_number, email, createdAt FROM user WHERE id = ?";
-  const values = [id];
-
-  return pool.query(query, values);
+const getDetailUserProfileById = (userId) => {
+  return prisma.profile.findUnique({
+    where: { userId },
+    select: {
+      email: true,
+      createdAt: true,
+      user: { select: { username: true, phone_number: true } },
+    },
+  });
 };
 
-const updateUserProfileById = (id, fields) => {
-  const setClause = Object.keys(fields)
-    .map((field) => `${field} = ?`)
-    .join(", ");
-  const values = Object.values(fields);
-  values.push(id);
-
-  const query = `UPDATE users SET ${setClause} WHERE id = ?`;
-
-  return pool.query(query, values);
+const updateUserProfileById = (userId, fields) => {
+  return prisma.profile.update({
+    where: { userId },
+    data: fields,
+  });
 };
 
 module.exports = {
   getUserProfileById,
   getDetailUserProfileById,
-  updateUserProfileById
-}
+  updateUserProfileById,
+};
