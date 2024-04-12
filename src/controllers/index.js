8db768b42966fa64509392db4d2d421c163e8dce
unknown <alwyyourbae@gmail.com>
2024-04-12 10:47:24 +0700

    add entry file for import export

---
+++ src/controllers/index.js
@@ -0,0 +1,11 @@
+const usersController = require("./users.controller");
+const authController = require("./auth.controller");
+const profileController = require("./profile.controller");
+const transactionsController = require("./transactions.controller");
+
+module.exports = {
+  usersController,
+  authController,
+  profileController,
+  transactionsController,
+};
