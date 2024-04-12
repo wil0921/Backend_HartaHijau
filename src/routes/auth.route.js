1ce48d465a7c9dda6579977441e2f18ff73124a2
unknown <alwyyourbae@gmail.com>
2024-04-12 10:51:12 +0700

    change import middleware path

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,20 +1,24 @@
-const router = require('express').Router();
-const { authController } = require('../controllers');
-const authMiddleware = require('../middleware/auth.middleware');
+const router = require("express").Router();
+const { authController } = require("../controllers");
+const { authMiddleware } = require("../middleware");
 
 // Route register
-router.post('/register', authController.register);
+router.post("/register", authController.register);
 
 // Route verify user
-router.post('/verify', authController.verifyOTP);
+router.post("/verify", authController.verifyOTP);
 
 // Route login
-router.post('/login', authController.login);
+router.post("/login", authController.login);
 
 // Route forgot password
-router.post('/forgotpassword', authController.forgotPassword);
+router.post("/forgotpassword", authController.forgotPassword);
 
 // Route secure user
-router.get('/secure', authMiddleware.authenticateToken, authController.secureAuth);
+router.get(
+  "/secure",
+  authMiddleware.authenticateToken,
+  authController.secureAuth
+);
 
 module.exports = router;
