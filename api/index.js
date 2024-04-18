9019b935bbd3e82e4ac19662798272900a1398a8
unknown <alwyyourbae@gmail.com>
2024-04-18 23:15:48 +0700

    add cors middleware

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors')
 const routes = require("../src/routes/");
 const { serverMiddleware } = require("../src/middleware");
 
@@ -7,6 +8,7 @@ const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
 app.use(serverMiddleware.logRequestTime);
 app.use(serverMiddleware.maintenanceMode);
 
