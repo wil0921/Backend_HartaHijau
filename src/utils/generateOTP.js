c999d36ab673c80ea8e855f6b16ec667ba0c738c
unknown <alwyyourbae@gmail.com>
2024-04-18 14:25:58 +0700

    change data type return

--- src/utils/generateOTP.js
+++ src/utils/generateOTP.js
@@ -1,5 +1,5 @@
 const generateOTP = () => {
-  return Math.floor(100000 + Math.random() * 900000);
+  return Math.floor(100000 + Math.random() * 900000).toString();
 };
 
 module.exports = generateOTP;
