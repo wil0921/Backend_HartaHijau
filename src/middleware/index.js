a5c334d59e53853df11f75a847450057b4d56ba6
unknown <alwyyourbae@gmail.com>
2024-04-22 08:17:36 +0700

    add errorMiddleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,4 +1,5 @@
 const authMiddleware = require("./auth.middleware");
 const serverMiddleware = require("./server.middleware");
+const errorMiddleware = require("./error.middleware");
 
-module.exports = { authMiddleware, serverMiddleware };
+module.exports = { authMiddleware, serverMiddleware, errorMiddleware };
