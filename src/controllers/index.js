35b0089b60f45eb121725b1dbeccb57662e15138
Nama Anda <emailanda@example.com>
2024-05-17 22:48:46 +0700

    add history controller

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,10 +2,12 @@ const usersController = require("./users.controller");
 const authController = require("./auth.controller");
 const profileController = require("./profile.controller");
 const transactionsController = require("./transactions.controller");
+const historyController = require("./history.controller");
 
 module.exports = {
   usersController,
   authController,
   profileController,
   transactionsController,
+  historyController,
 };
